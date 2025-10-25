deab4c8605cac89e33f178dcf2ebcff803756faa
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-10-25 16:16:55 +0900

    add:ログアウト機能

--- react-firebase-blog/src/components/Logout.js
+++ react-firebase-blog/src/components/Logout.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { auth } from "../firebase";
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
-const Logout = () => {
+const Logout = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const logout = () =>  {
+    // ログアウト
+    signOut(auth).then(() => {
+      // ローカルストレージをクリア
+      localStorage.clear();    
+      // 状態をfalseにする
+      setIsAuth(false);
+      // ログインページにリダイレクト
+      navigate("/login");
+    })
+  };
+
   return (
-    <div>Logout</div>
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>ログアウト</button>
+    </div>
   )
 }
 
