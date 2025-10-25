9510815c4e3389654d8fe326fb96b91e023c58db
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-10-25 15:55:44 +0900

    add:ログイン実装

--- react-firebase-blog/src/components/Login.js
+++ react-firebase-blog/src/components/Login.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginInWithGoogle = () =>  {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result)=>{
+      // ローカルストレージに保存
+      localStorage.setItem("isAuth", true);
+      // 状態をtrueにする
+      setIsAuth(true);
+      // ホームへリダイレクト
+      navigate("/");
+    });
+  };
+
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
   )
 }
 
