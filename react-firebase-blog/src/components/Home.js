cd06e316311e7d379d27de2a2373fa2026685867
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-11-08 14:33:43 +0900

    add:ブログデータの取得

--- react-firebase-blog/src/components/Home.js
+++ react-firebase-blog/src/components/Home.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import { collection, doc, getDocs } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { db } from "../firebase";
 import "./Home.css"
 
 const Home = () => {
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // console.log(data);
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+    getPosts();
+  }, []);
+  
   return (
     <>
       <div className='homePage'>
